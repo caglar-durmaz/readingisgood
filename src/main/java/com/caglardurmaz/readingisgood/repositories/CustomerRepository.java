36ec9c88de9cabbd0b3a72d63533857a1ac65321
Caglar DURMAZ <caglar.durmaz@vodafone.com>
2021-10-09 17:52:20 +0300

    h2 entities and repository implementation

---
+++ src/main/java/com/caglardurmaz/readingisgood/repositories/CustomerRepository.java
@@ -0,0 +1,6 @@
+package com.caglardurmaz.readingisgood.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<CustomerRepository, Long> {
+}
---
+++ src/main/java/com/caglardurmaz/readingisgood/repositories/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.caglardurmaz.readingisgood.repositories;
+
+import com.caglardurmaz.readingisgood.domain.Order;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OrderRepository extends CrudRepository<Order, Long> {
+}
